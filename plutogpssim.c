3d4b81cb661b8df7352be151d9872134a5921cbf
Mictronics <github@mictronics.de>
2018-03-25 10:30:25 +0200

    Added support to build on MAC OS.

--- plutogpssim.c
+++ plutogpssim.c
@@ -22,6 +22,11 @@
 #include <curl/curl.h>
 #include <iio.h>
 #include <ad9361.h>
+#if defined(__MACH__) || defined(__APPLE__)
+#include <mach/mach_init.h>
+#include <mach/thread_act.h>
+#include <mach/mach_port.h>
+#endif
 #include "gpssim.h"
 
 /*
@@ -41,6 +46,27 @@
 #define GHZ(x) ((long long)(x*1000000000.0 + .5))
 #define NUM_SAMPLES 260000
 #define BUFFER_SIZE (NUM_SAMPLES * 2 * sizeof(int16_t))
+
+#if defined(__MACH__) || defined(__APPLE__)
+typedef struct cpu_set {
+    uint32_t count;
+} cpu_set_t;
+
+static inline void
+CPU_ZERO(cpu_set_t *cs) {
+    cs->count = 0;
+}
+
+static inline void
+CPU_SET(int num, cpu_set_t *cs) {
+    cs->count |= (1 << num);
+}
+
+static inline int
+CPU_ISSET(int num, cpu_set_t *cs) {
+    return (cs->count & (1 << num));
+}
+#endif
 
 struct stream_cfg {
     long long bw_hz; // Analog banwidth in Hz
@@ -1648,6 +1674,24 @@ static void handle_sig(int sig)
     pthread_mutex_destroy(&plutotx.data_mutex);
     pthread_cond_destroy(&plutotx.data_cond);
 }
+
+#if defined(__MACH__) || defined(__APPLE__)
+static int pthread_setaffinity_np(pthread_t thread, size_t cpu_size,
+        cpu_set_t *cpu_set) {
+    thread_port_t mach_thread;
+    size_t core = 0;
+
+    for (core = 0; core < 8 * cpu_size; core++) {
+        if (CPU_ISSET(core, cpu_set)) break;
+    }
+    printf("binding to core %ld\n", core);
+    thread_affinity_policy_data_t policy = {core};
+    mach_thread = pthread_mach_thread_np(thread);
+    thread_policy_set(mach_thread, THREAD_AFFINITY_POLICY,
+            (thread_policy_t) & policy, 1);
+    return 0;
+}
+#endif
 
 // Set affinity of calling thread to specific core on a multi-core CPU
 static int thread_to_core(int core_id) {
